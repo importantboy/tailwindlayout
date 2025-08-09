4cf9491bda6fc634c7a72e122dab1f80fa7d7749
rahul kumar <importantinbox123@gmail.com>
2025-08-09 14:33:27 +0530

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Paragraph from './paragraph';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className='w-full h-screen bg-emerald-100 container m-auto relative'>
+       <Navbar />
+       {/* <Paragraph /> */}
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { FiMenu } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
+
+function Navbar() {
+  const [open, setopen] = useState(false);
+  return (
+    <>
+      {/* open button  */}
+      <h2
+        className={`md:hidden text-4xl absolute right-10 top-5 cursor-pointer z-40 text-emerald-800  ${
+          open && "hidden"
+        }`} 
+        onClick={() => setopen(!open)}
+      >
+        <FiMenu />{" "}
+      </h2>
+       {/* menu container  */}
+      <div
+        className={`fixed inset-0 md:static md:flex w-full h-screen  md:bg-transparent ${
+          open ? `bg-black/50` : `bg-transparent pointer-events-none`
+        } overflow-x-hidden transition-opacity duration-500`}
+      >
+        {/* {menu items container}  */}
+        <div
+          className={`w-1/2 md:w-full h-screen flex-col md:flex-row md:h-20 justify-start absolute md:static md:translate-x-0 right-0 md:right-auto ${
+            open ? `translate-x-0` : `translate-x-full`
+          } bg-emerald-700 transition-transform duration-500 ease-in-out
+    text-emerald-50 flex md:justify-between px-5 items-center`}
+       onClick={e => e.stopPropagation()}
+        >
+          {/* {menu heading}  */}
+          <h2 className="text-xl sm:text-2xl tracking-wide absolute md:static top-5 left-5">
+            Mywebsite
+          </h2>
+           {/* menu close button  */}
+          <h2
+            className={`md:hidden absolute text-3xl font-bold right-10 top-5 cursor-pointer hover:text-emerald-500`}
+            onClick={() => setopen(!open)}
+          >
+            <IoMdClose />
+          </h2>
+          {/* menu navigation links  */}
+          <nav
+            className="text-lg md:text-xl list-none flex gap-4 capitalize 
+      absolute md:static top-20 hover:cursor-pointer [&>li]:hover:text-emerald-400 flex-col md:flex-row text-center"
+          >
+            <li>home</li>
+            <li>about</li>
+            <li>contact us</li>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
